a7f1667f0f49c263b1482e4149b0a9dd666915de
Casper Wang <casperwang@casperwang.dev>
2024-05-16 15:05:34 +0800

    define api functions

--- backend/model_test.py
+++ backend/model_test.py
@@ -8,74 +8,11 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.patcher = patch('model.get_client', return_value=mongomock.MongoClient())
         self.mock_client = self.patcher.start()
-        self.db = self.mock_client()["SplitwiseWeb"]
-        global users_collection, transaction_sets_collection, transactions_collection
-        users_collection = self.db['users']
-        transaction_sets_collection = self.db['transaction_sets']
-        transactions_collection = self.db['transactions']
-
-        self.user_id = dict()
-        self.user_id["qingyun"] = create_user("qingyun", "qingyun@gmail.com")
-        self.user_id["casperwang"] = create_user("casperwang", "casperwang@gmail.com")
-        self.user_id["thomaswang"] = create_user("thomaswang", "thomaswang@gmail.com")
-        self.set_id = dict()
-        self.set_id["friend_test"] = make_friend(self.user_id["qingyun"], self.user_id["casperwang"])
-        self.set_id["group_test"] = create_group("group_test", [self.user_id["qingyun"], self.user_id["casperwang"]])
+        self.db = self.mock_client()["CNL"]
+        global meeting_collection
 
     def tearDown(self):
         self.patcher.stop()
-
-    def test_create_user(self):
-        self.assertEqual("qingyun", users_collection.find_one({"_id": self.user_id["qingyun"]})["name"])
-        self.assertEqual(3, users_collection.count_documents({}))
-    
-    def test_make_friend(self):
-        self.assertSequenceEqual(
-            [self.user_id["qingyun"], self.user_id["casperwang"]],
-            transaction_sets_collection.find_one({"_id": self.set_id["friend_test"]})["user_list"]
-        )
-        self.assertEqual(
-            self.set_id["friend_test"],
-            users_collection.find_one({"_id": self.user_id["qingyun"]})["friend_list"][0]
-        )
-    
-    def test_create_group(self):
-        self.assertEqual(
-            "group_test",
-            transaction_sets_collection.find_one({"_id": self.set_id["group_test"]})["name"]
-        )
-        self.assertEqual(
-            self.set_id["group_test"],
-            users_collection.find_one({"_id": self.user_id["casperwang"]})["group_list"][0]
-        )
-    
-    def test_create_transaction(self):
-        transaction = Transaction(
-            "test1",
-            self.set_id["friend_test"],
-            0,
-            "2024-04-21",
-            [self.user_id["qingyun"], self.user_id["casperwang"]],
-            "payment",
-            [(self.user_id["qingyun"], 100)],
-            [(self.user_id["casperwang"], "exact", 100)]
-        )
-        tid = create_transaction(transaction)
-        self.assertEqual(
-            -100,
-            transactions_collection.find_one({"_id": tid})["balance"][self.user_id["casperwang"]]
-        )
-        self.assertEqual(
-            100,
-            transaction_sets_collection.find_one({"_id": self.set_id["friend_test"]})["balance"][self.user_id["qingyun"]]
-        )
-    
-    def test_add_user(self):
-        add_user(self.user_id["thomaswang"], self.set_id["group_test"])
-        self.assertEqual(
-            3,
-            len(transaction_sets_collection.find_one({"_id": self.set_id["group_test"]})["user_list"])
-        )
 
 if __name__ == '__main__':
     unittest.main()
--- backend/server.py
+++ backend/server.py
@@ -7,199 +7,58 @@ from model import *
 
 app = FastAPI()
 
-class UserData(BaseModel):
-    username: str
-    email: str
-
-@app.post("/create_user/")
-def api_create_user(user: UserData):
-    user_id = create_user(user.username, user.email)
-    return {"user_id": user_id}
-
-class FriendData(BaseModel):
-    userA: str
-    userB: str
-
-@app.post("/make_friend/")
-def api_make_friend(friend: FriendData):
-    set_id = make_friend(friend.userA, friend.userB)
-    return {"set_id": set_id}
-
-class GroupData(BaseModel):
-    groupname: str
-    user_list: List[str]
-
-@app.post("/create_group/")
-def api_create_group(group: GroupData):
-    set_id = create_group(group.groupname, group.user_list)
-    return {"set_id": set_id}
-
-class TransactionData(BaseModel):
+class MeetingData(BaseModel):
     name: str
-    set_id: str
-    label: int
-    date: str
-    user_list: List[str]
-    split_type: str
-    payers: Optional[List[Tuple[str, float]]] = list()
-    owners: Optional[List[Tuple[str, str, float]]] = list()
-    balance: Optional[Dict[str, float]] = dict()
+    url: str
+    time: Tuple[str, str]
+    type: str
+    host_id: str
+    user_ids: List[str]
+    gps: Optional[str]
 
-@app.post("/create_transaction/")
-def api_create_transaction(transaction: TransactionData):
-    transaction_id = create_transaction(Transaction(
-        transaction.name,
-        transaction.set_id,
-        transaction.label,
-        transaction.date,
-        transaction.user_list,
-        transaction.split_type,
-        payers=transaction.payers,
-        owners=transaction.owners,
-        balance=transaction.balance))
-    return {"transaction_id": transaction_id}
-
-class UserGroup(BaseModel):
-    user_id: str
-    set_id: str
-
-@app.post("/add_user/")
-def api_add_user(usergroup: UserGroup):
-    res = add_user(usergroup.user_id, usergroup.set_id)
-    return {"res": res}
-
-@app.get("/get_user/")
-def api_get_user(email: str):
-    user_data = get_email_user(email)
-    user = {
-        "id": user_data["_id"],
-        "name": user_data["name"],
-        "image": None
+@app.post("/create_meeting/")
+def api_create_meeting(meeting: MeetingData):
+    meeting_id = create_meeting(Meeting(
+        name=meeting.name,
+        url=meeting.url,
+        time=meeting.time,
+        meeting_type=meeting.type,
+        host_id=meeting.host_id,
+        user_ids=meeting.user_ids,
+        gps=meeting.gps
+    ))
+    return {
+        "meeting_id": meeting_id
     }
-    return user
 
-@app.get("/get_friends/")
-def api_get_friends(user_id: str):
-    friend_list = get_user(user_id)["friend_list"]
-    friends = list()
-    for set_id in friend_list:
-        set = get_transaction_set(set_id)
-        friend_id = set["user_list"][0] if set["user_list"][0] != user_id else set["user_list"][1]
-        friends.append({
-            "id": set_id,
-            "name": get_user(friend_id)["name"],
-            "balance": set["balance"][user_id],
-            "image": None
-        })
-    return friends
+@app.get("/get_onsite_qrcode/")
+def api_get_onsite_qrcode(meeting_id: str):
+    meeting = get_meeting(id=meeting_id)
+    assert(meeting.type == "onsite")
+    qrcode = get_qrcode(meeting.qrcodes[-1])
+    return qrcode.url
 
-@app.get("/get_groups/")
-def api_get_groups(user_id: str):
-    group_list = get_user(user_id)["group_list"]
-    groups = list()
-    for set_id in group_list:
-        set = get_transaction_set(set_id)
-        groups.append({
-            "id": set_id,
-            "name": set["name"],
-            "balance": set["balance"][user_id],
-            "image": None
-        })
-    return groups
+@app.get("/get_user_status/")
+def api_get_user_status(meeting_id: str):
+    meeting = get_meeting(id=meeting_id)
+    if meeting.type == "onsite":
+        pass
+    elif meeting.type == "online":
+        pass
 
-@app.get("/get_friend/")
-def api_get_friend(user_id: str, set_id: str):
-    set = get_transaction_set(set_id)
-    friend_id = set["user_list"][0] if set["user_list"][0] != user_id else set["user_list"][1]
-    friend = {
-        "name": get_user(friend_id)["name"],
-        "balance": set["balance"][user_id],
-        "image": None,
-        "transactions": list()
-    }
-    for transaction_id in set["transaction_list"]:
-        transaction = get_transaction(transaction_id)
-        total_amount = sum([amount for _, amount in transaction["payers"]])
-        friend["transactions"].append({
-            "name": transaction["name"],
-            "label": transaction["label"],
-            "date": transaction["date"],
-            "payer": {
-                "name": get_user(transaction["payers"][0][0])["name"],
-                "amount": total_amount
-            },
-            "balance": transaction["balance"][user_id] if user_id in transaction["balance"] else 0
-        })
-    friend["transactions"].sort(key=lambda transaction: transaction["date"])
-    return friend
+@app.post("/join_meeting/")
+def api_join_meeting(user_id: str, meeting_url: str):
+    meeting = get_meeting(url=meeting_url)
+    pass
 
-@app.get("/get_group/")
-def api_get_group(user_id: str, set_id: str):
-    set = get_transaction_set(set_id)
-    group = {
-        "name": set["name"],
-        "balance": set["balance"][user_id],
-        "image": None,
-        "transactions": list()
-    }
-    for transaction_id in set["transaction_list"]:
-        transaction = get_transaction(transaction_id)
-        total_amount = sum([amount for _, amount in transaction["payer"]])
-        group["transactions"].append({
-            "name": transaction["name"],
-            "label": transaction["label"],
-            "date": transaction["date"],
-            "payer": {
-                "name": get_user(transaction["payer"][0][0])["name"] if len(transaction["payer"]) == 1 else len(transaction["payer"]),
-                "amount": total_amount
-            },
-            "balance": transaction["balance"][user_id] if user_id in transaction["balance"] else 0
-        })
-    group["transactions"].sort(key=lambda transaction: transaction["date"])
-    return group
+@app.get("/get_online_qrcode/")
+def api_get_online_qrcode(meeting_url: str):
+    pass
 
-@app.get("/get_transaction/")
-def api_get_transaction(id: str):
-    transaction_data = get_transaction(id)
-    transaction = {
-        "name": transaction_data["name"],
-        "set_id": transaction_data["set_id"],
-        "label": transaction_data["label"],
-        "date": transaction_data["date"],
-        "user_list": transaction_data["user_list"],
-        "split_type": transaction_data["split_type"],
-    }
-    if transaction["split_type"] == "balance":
-        transaction["balance"] = [{
-            "name": get_user(user_id),
-            "user_id": user_id,
-            "amount": transaction_data["balance"][user_id]
-        } for user_id in transaction_data["balance"].keys()]
-    elif transaction["split_type"] == "payment":
-        transaction["payers"] = [{
-            "name": get_user(user_id),
-            "user_id": user_id,
-            "amount": amount
-        } for user_id, amount in transaction_data["payers"]]
-        transaction["owners"] = [{
-            "name": get_user(user_id),
-            "user_id": user_id,
-            "type": own_type,
-            "value": value
-        } for user_id, own_type, value in transaction_data["owners"]]
-    return transaction
+@app.post("/onsite_sign/")
+def api_onsite_sign():
+    pass
 
-@app.post("/del_user/")
-def api_del_user(user_id: str):
-    result = del_user(user_id)
-    return {"result": result}
-
-@app.post("/del_transaction_set/")
-def api_del_set(set_id: str):
-    result = del_transaction_set(set_id)
-    return {"result": result}
-
-@app.post("/del_transaction/")
-def api_del_transaction(transaction_id: str):
-    result = del_transaction(transaction_id)
-    return {"result": result}
+@app.post("/online_sign/")
+def api_online_sign():
+    pass
