3fb96a7bb56132af0b3b571c44de9e909e6aa7bf
jingjin412 <b10204022@g.ntu.edu.tw>
2024-05-27 00:07:56 +0800

    Update App.js

--- user/src/App.js
+++ user/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from "./pages/login"; // 確保您已經有了Login組件
 import GPS from "./pages/GPS";
+import SUCCESS from "./pages/Success";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/GPS" element={<GPS />} />
+          <Route path="/Success" element={<SUCCESS />} />
         </Routes>
       </div>
     </Router>
