af8d8a8c146171bb6e43771cfa543e509401f976
terry489490 <terry489490@gmail.com>
2024-05-29 18:18:20 +0800

    fix to_uid

--- backend/server.py
+++ backend/server.py
@@ -87,7 +87,7 @@ def api_get_user_status(meeting_id: str):
 @app.post("/join_meeting/")
 def api_join_meeting(user_id: str, meeting_url: str):
     meeting = get_meeting(url=meeting_url)
-    if user_id in meeting.user_ids:
+    if to_uid(user_id) in meeting.user_ids:
         raise HTTPException(status_code=409, detail="User already registered")
     add_user(meeting._id, user_id)
     return {"message": "User added successfully"}
