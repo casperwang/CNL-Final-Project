2bcbdcbd393c700ba7f737bc2496d93202aca77b
terry489490 <terry489490@gmail.com>
2024-05-29 16:09:20 +0800

    debug

--- backend/utils.py
+++ backend/utils.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from numpy import sin, cos, arccos, pi, round
 
 def random_time_points(start_time, end_time, num_points):
-    delta = end_time - start_time
+    delta = int(end_time) - int(start_time)
     delta_seconds = int(delta.total_seconds())
     random_seconds = sorted(random.randint(0, delta_seconds // 60 - 1) * 60 for _ in range(num_points))
     time_points = [start_time + timedelta(seconds=sec) for sec in random_seconds]
